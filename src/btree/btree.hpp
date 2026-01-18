c30cf158c454678ad10db8c39df97d18afa21945
William Gaston <z5591798@ad.unsw.edu.au>
2026-01-18 21:58:58 +1100

    Add function definitions for btree

--- src/btree/btree.hpp
+++ src/btree/btree.hpp
@@ -8,9 +8,20 @@ class Btree {
 public:
     Btree();
     void insert(int64_t key);
-    bool find(int64_t key);
+    bool find(int64_t key);     
+    void remove(int64_t key);
+    void traverse() const;
+    int64_t getCount() const;
+    int64_t getHeight() const;
+    bool isEmpty() const;
 private:
     unique_ptr<BtreeNode> root; // root node of the tree
     int64_t count; //  num items
     int64_t height; // height from root to leaf node
+
+    bool findRecurse(BtreeNode* node, int64_t key);
+    int insertRecurse(BtreeNode* node, int64_t key);
+    void splitChild(BtreeNode* node, int index);
+    void removeRecurse(BtreeNode* node, int64_t key);
+    void traverseRecurse(const BtreeNode* node) const;
 }
--- src/btree/node.hpp
+++ src/btree/node.hpp
@@ -9,9 +9,37 @@ public:
 
     void addDataItem(int newItem); // add a data item to the current btree node
     void addChildNode(unique_ptr<BtreeNode> newNode); // add a btree node to children array
+
+    int findKeyIndex(int64_t key) const; // find index of key location
+    bool containsKey(int64_t key) const; // check if key exists
+
+    bool isFull() const;
+    bool isMinimal() const;
+    bool isLead() const;
+    int getNumDataItems() const;
+
+    int64_t getKey(int index) const;
+    BtreeNode* getChild(int index) const;
+
+    unique_ptr<BtreeNode> split(int& medianKey); // returns right half
+
+    void insertNotFull(int64_t key);
+
+    void removeKey(int index);
+    void removeChild(int index);
+    int64_t getPredecessor(int index) const;
+    int64_t getSuccessor(int index) const;
+    void merge(int index);
+    void borrowFromPrev(int index);
+    void borrowFromNext(int index);
+    void fill(int index); // fixes node underflow
 private:
     bool isLeaf;
     int64_t numDataItems;
-    vector<int> numDataItems;
+    vector<int> dataItems;
     vector<unique_ptr<BtreeNode>> childNodes;
+
+    static const int ORDER = 10;
+    static const int MAX_KEYS = ORDER - 1;
+    static const int MIN_KEYS = (ORDER - 1 ) / 2;
 }
